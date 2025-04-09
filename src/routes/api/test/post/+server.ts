33cebe4480efbec3ee208833ab62f887938e2b16
rohanbabuam <rohanbabu.am@renderpub.com>
2025-04-09 12:15:41 +0530

    testing r2

--- src/routes/api/test/post/+server.ts
+++ src/routes/api/test/post/+server.ts
@@ -30,33 +30,33 @@ export async function POST(event: RequestEvent) {
 		}
 	const bucket = event.platform?.env.MADMODS_R2;
 
-	// --- 2. Get Inputs ---
-    let inputs:any;
-    try {
-        inputs = await event.request.json();
-        if (!inputs) {
-            throw new Error("Missing input data in request body");
-        }
-    } catch (err:any) {
-        console.error("Failed to parse request JSON:", err);
-        throw error(400, `Bad Request: ${err.message}`);
-    }
-    let fileURL_toUpload = inputs.fileURL_ToUpload;
-    let r2key = inputs.r2key;
+	// // --- 2. Get Inputs ---
+    // let inputs:any;
+    // try {
+    //     inputs = await event.request.json();
+    //     if (!inputs) {
+    //         throw new Error("Missing input data in request body");
+    //     }
+    // } catch (err:any) {
+    //     console.error("Failed to parse request JSON:", err);
+    //     throw error(400, `Bad Request: ${err.message}`);
+    // }
+    // let fileURL_toUpload = inputs.fileURL_ToUpload;
+    // let r2key = inputs.r2key;
 
-	// --- 3. Fetch the File from the URL ---
-    console.log("Fetching file from fileURL...");
-    const fetchResponse = await fetch(fileURL_toUpload);
+	// // --- 3. Fetch the File from the URL ---
+    // console.log("Fetching file from fileURL...");
+    // const fetchResponse = await fetch(fileURL_toUpload);
 
-    if (!fetchResponse.ok) {
-        console.error(`Failed to fetch file from ${fileURL_toUpload}. Status: ${fetchResponse.status} ${fetchResponse.statusText}`);
-        const errorBody = await fetchResponse.text().catch(() => 'Could not read error body');
-        throw error(502, `Failed to download generated file: ${fetchResponse.statusText}. Body: ${errorBody}`);
-    }
+    // if (!fetchResponse.ok) {
+    //     console.error(`Failed to fetch file from ${fileURL_toUpload}. Status: ${fetchResponse.status} ${fetchResponse.statusText}`);
+    //     const errorBody = await fetchResponse.text().catch(() => 'Could not read error body');
+    //     throw error(502, `Failed to download generated file: ${fetchResponse.statusText}. Body: ${errorBody}`);
+    // }
 
-	// --- 4. Get File Data and Metadata ---
-	const fileData = await fetchResponse.arrayBuffer(); // Get content as ArrayBuffer
-	const contentType = fetchResponse.headers.get('content-type') || 'application/octet-stream';
+	// // --- 4. Get File Data and Metadata ---
+	// const fileData = await fetchResponse.arrayBuffer(); // Get content as ArrayBuffer
+	// const contentType = fetchResponse.headers.get('content-type') || 'application/octet-stream';
 
 	return new Response(JSON.stringify(showOrigin), {
 		status: 200
