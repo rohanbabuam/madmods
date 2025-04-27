1745614fe9ccea4fed96390ea3f7d97a3519d338
rohanbabuam <rohanbabu.am@renderpub.com>
2025-04-27 15:03:22 +0530

    asd

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,20 +2,22 @@
 import type { LayoutServerLoad } from './$types'; // Correct type for server layout load
 
 let supabase_url, supabase_key;
-try{
-    const { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } = await import('$env/static/public');
-    // const { PRIVATE_GEMINI_API_KEY } = await import('$env/static/private');
-    supabase_url = PUBLIC_SUPABASE_URL;
-    supabase_key = PUBLIC_SUPABASE_ANON_KEY;
-  }
-  catch (err:any) {
-    console.error("Error during env var import:", err);
-    // geminiToken = event.platform?.env.PRIVATE_GEMINI_API_KEY;
-    supabase_url = event.platform?.env.PUBLIC_SUPABASE_URL;
-    supabase_key = event.platform?.env.PUBLIC_SUPABASE_ANON_KEY;
-  }
 
-export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
+
+export const load: LayoutServerLoad = async ({ platform, locals: { getSession } }) => {
+    try{
+        const { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } = await import('$env/static/public');
+        // const { PRIVATE_GEMINI_API_KEY } = await import('$env/static/private');
+        supabase_url = PUBLIC_SUPABASE_URL;
+        supabase_key = PUBLIC_SUPABASE_ANON_KEY;
+      }
+      catch (err:any) {
+        console.error("Error during env var import:", err);
+        // geminiToken = event.platform?.env.PRIVATE_GEMINI_API_KEY;
+        supabase_url = platform?.env.PUBLIC_SUPABASE_URL;
+        supabase_key = platform?.env.PUBLIC_SUPABASE_ANON_KEY;
+      }
+
     // Get the session from the helper function we defined in hooks.server.ts
     const session = await getSession();
 
